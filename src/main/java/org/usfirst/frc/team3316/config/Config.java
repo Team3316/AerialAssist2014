f80321a1e8922dd15bf5158b09dd73d21523022a
Jonathan Ohayon <me@jonathano.com>
2018-06-26 00:47:10 +0300

    Added config

---
+++ src/main/java/org/usfirst/frc/team3316/config/Config.java
@@ -0,0 +1,386 @@
+package org.usfirst.frc.team3316.config;
+
+import java.util.Hashtable;
+
+public class Config {
+  public static Hashtable<String, Object> variablesB;
+  public static Hashtable<String, Object> constantsB;
+
+  public static Hashtable<String, Object> variablesA;
+  public static Hashtable<String, Object> constantsA;
+
+  static {
+    variablesB = new Hashtable<String, Object>();
+    constantsB = new Hashtable<String, Object>();
+
+    variablesA = new Hashtable<String, Object>();
+    constantsA = new Hashtable<String, Object>();
+
+    initConfig();
+    IO.initIO();
+  }
+
+  public static void addToConstantsA(String key, Object value) {
+    System.out.println("Trying to add to constants A: key " + key + " value " + value);
+
+    if (constantsA.containsKey(key)) {
+      constantsA.replace(key, value);
+    } else {
+      constantsA.put(key, value);
+    }
+  }
+
+  public static void addToVariablesA(String key, Object value) {
+    System.out.println("Trying to add to variables A: key " + key + " value " + value);
+
+    if (variablesA.containsKey(key)) {
+      variablesA.replace(key, value);
+    } else {
+      variablesA.put(key, value);
+    }
+  }
+
+  public static void addToConstantsB(String key, Object value) {
+    System.out.println("Trying to add to constants B: key " + key + " value " + value);
+
+    if (constantsB.containsKey(key)) {
+      constantsB.replace(key, value);
+    } else {
+      constantsB.put(key, value);
+    }
+  }
+
+  public static void addToVariablesB(String key, Object value) {
+    System.out.println("Trying to add to variables B: key " + key + " value " + value);
+
+    if (variablesB.containsKey(key)) {
+      variablesB.replace(key, value);
+    } else {
+      variablesB.put(key, value);
+    }
+  }
+
+  public static void addToConstants(String key, Object value) {
+    addToConstantsA(key, value);
+    addToConstantsB(key, value);
+  }
+
+  public static void addToVariables(String key, Object value) {
+    addToVariablesA(key, value);
+    addToVariablesB(key, value);
+  }
+
+  /*
+   * NOTE: constants and variables that are common to both robot A and robot B
+   * should be added with addToConstants() or addToVariables()
+   *
+   * Use different constants and variables for the two robots only if there is a
+   * difference. TestModeStuff
+   */
+  private static void initConfig() {
+    /*
+     * Human IO
+     */
+    {
+      /*
+       * Constants
+       */
+      {
+        /*
+         * Joysticks
+         */
+        {
+          addToConstants("JOYSTICK_LEFT", 0);
+          addToConstants("JOYSTICK_RIGHT", 1);
+          addToConstants("JOYSTICK_OPERATOR", 2);
+        }
+
+        /*
+         * Buttons and axis
+         */
+        {
+          // Joystick operator
+          addToVariables("button_Elevetor_Top", 4);
+          addToVariables("button_Elevetor_Bottom", 1);
+          addToVariables("button_Elevetor_Switch", 2);
+          addToVariables("button_Elevetor_Scale", 3);
+
+          addToVariables("button_Elevator_Joystick_Toggle", 10);
+          addToVariables("elevator_Joystick_Axis", 5);
+
+          addToVariables("button_Climb_Up", 9);
+
+          addToVariables("button_Collection", 6);
+          addToVariables("button_Ejection", 5);
+
+          addToVariables("button_Holder_RollIn", 8);
+          addToVariables("button_Holder_RollOut", 7);
+
+          addToVariables("button_Chassis_Break_Toggle", 1);
+          addToVariables("button_Chassis_DriveOneAxis", 3);
+          addToVariables("axis_Chassis_DriveOneAxis1", 3);
+        }
+      }
+    }
+
+    /*
+     * RobotIO
+     */
+    {
+      /*
+       * Constants
+       */
+      addToConstants("CURRENT_CONTROL_COUNTER", 10);
+
+      {
+        /*
+         * Chassis
+         */
+        addToConstantsA("CHASSIS_MOTOR_LEFT_REVERSE", false);
+        addToConstantsA("CHASSIS_MOTOR_RIGHT_REVERSE", true);
+
+        addToConstantsB("CHASSIS_MOTOR_LEFT_REVERSE", false);
+        addToConstantsB("CHASSIS_MOTOR_RIGHT_REVERSE", true);
+
+        addToVariables("chassis_Joystick_Right_Axis", 1);
+        addToVariables("chassis_Joystick_Left_Axis", 5);
+
+        addToConstants("CHASSIS_LEFT_ENCODER_REVERSE", false);
+        addToConstants("CHASSIS_RIGHT_ENCODER_REVERSE", true);
+
+        addToConstants("CHASSIS_ENCODERS_DISTANCE_PER_PULSE", 0.00124224); // in meters
+      }
+
+      {
+        /*
+         * Elevator
+         */
+        addToConstantsA("ELEVATOR_ENCODER_DISTANCE_PER_PULSE", 0.0001092); // in meters
+        addToConstantsB("ELEVATOR_ENCODER_DISTANCE_PER_PULSE", 0.0001092); // in meters
+        addToConstants("ELEVATOR_MOTORS_REVERSE", false);
+        addToConstants("ELEVATOR_MOTORS_MAX_VOLTAGE", 35.0);
+      }
+    }
+
+    /*
+     * Chassis
+     */
+    {
+      /*
+       * Constants
+       */
+      {
+      }
+
+      /*
+       * Variables
+       */
+      {
+        addToVariables("chassis_TankDrive_DeadBand", 0.05);
+
+        addToVariables("chassis_SpeedFactor_Higher", -1.0);
+        addToVariables("chassis_SpeedFactor_Lower", -0.3);
+
+        addToVariablesA("chassis_SpeedFactor_Current", -1.0); // the default value
+        addToVariablesB("chassis_SpeedFactor_Current", -1.0); // the default value
+
+        addToVariables("chassis_TankDrive_InvertX", true);
+        addToVariables("chassis_TankDrive_InvertY", false); // false value for xbox
+
+        addToVariables("chassis_LowPassFilter_LowestValue", 0.05);
+      }
+
+      /*
+       * Drive Distance
+       */
+      {
+        addToVariables("chassis_DriveDistance_PID_Tolerance", 0.025);
+
+        // PID
+        // BY CAMERA
+        addToVariables("chassis_DriveByCamera_PID_KP", 0.0);
+        addToVariables("chassis_DriveByCamera_PID_KI", 0.0);
+        addToVariables("chassis_DriveByCamera_PID_KD", 0.0);
+
+        // BY ENCODERS
+        {
+          // Drive
+          addToVariables("chassis_DriveDistance_PID_DRIVE_KP", 1000.0);
+          addToVariables("chassis_DriveDistance_PID_DRIVE_KI", 0.0);
+          addToVariables("chassis_DriveDistance_PID_DRIVE_KD", 0.0);
+
+          // Yaw
+          addToVariables("chassis_DriveDistance_PID_YAW_KP", 270.0);
+          addToVariables("chassis_DriveDistance_PID_YAW_KI", 0.0);
+          addToVariables("chassis_DriveDistance_PID_YAW_KD", 0.0);
+          addToVariables("chassis_DriveDistance_PID_YAW_KF", 0.0);
+        }
+      }
+
+      /*
+       * TURN BY GYRO
+       */
+      {
+        // PID
+        addToVariables("chassis_TurnByGyro_PID_Tolerance", 3.0);
+
+        addToVariables("chassis_TurnByGyro_PID_KP", 30.0);
+        addToVariables("chassis_TurnByGyro_PID_KI", 0.0);
+        addToVariables("chassis_TurnByGyro_PID_KD", 10.0);
+
+        addToVariables("chassis_TurnByGyro_VelocityFilter", 0.025);
+
+        // BangBang
+        addToVariables("chassis_TurnByGyro_UpVoltage", 0.4);
+        addToVariables("chassis_TurnByGyro_DownVoltage", -0.4);
+        addToVariables("chassis_TurnByGyro_CurrentInitYaw", 0.0); // Initial value
+      }
+
+      /*
+       * Set Constant Voltage
+       */
+      {
+        addToVariables("chassis_SetConstantVoltage_Voltage", 0.0);
+      }
+
+      /*
+       * SPEED PID
+       */
+      {
+        addToVariables("chassis_Speed_PID_Left_KP", 10.0 / 1000.0);
+        addToVariables("chassis_Speed_PID_Left_KD", 0.0);
+        addToVariables("chassis_Speed_PID_Left_KI", 0.0);
+        addToVariables("chassis_Speed_PID_Left_KF", 0.0);
+        addToVariables("chassis_Speed_PID_Right_KP", 10.0 / 1000.0);
+        addToVariables("chassis_Speed_PID_Right_KI", 0.0);
+        addToVariables("chassis_Speed_PID_Right_KD", 0.0);
+        addToVariables("chassis_Speed_PID_Right_KF", 0.0);
+
+        addToVariables("chassis_Speed_PID_Tolerance", 0.05);
+      }
+
+      /*
+       * YAW PID
+       */
+      {
+        addToVariables("chassis_Yaw_PID_KP", 50.0 / 1000.0);
+        addToVariables("chassis_Yaw_PID_KD", 0.0);
+        addToVariables("chassis_Yaw_PID_KI", 0.0);
+      }
+
+      /*
+       * PATH FOLLOWER
+       */
+      {
+        addToVariables("chassis_PF_Steptime", 0.1); // in seconds
+        addToVariables("chassis_PF_Totaltime", 5.0); // in seconds
+
+        addToConstants("CHASSIS_PF_TRACKWIDTH", 1.94); // in feet
+      }
+
+    }
+
+    /*
+     * Intake
+     */
+    {
+      addToVariables("intake_rollIn_voltage", 0.7);
+      addToVariablesA("intake_rollOut_voltage", -1.0);
+
+      addToVariablesB("intake_rollOut_voltage", 1.0);
+
+      addToVariables("intake_shaken_stepTime", 750.0);
+      addToVariables("intake_shaken_stopTime", 200.0);
+
+      addToVariablesA("intake_directionalRollIn_leftVoltage", 1.0);
+      addToVariablesA("intake_directionalRollIn_rightVoltage", 0.4);
+
+      addToVariablesB("intake_directionalRollIn_leftVoltage", -1.0);
+      addToVariablesB("intake_directionalRollIn_rightVoltage", -0.4);
+    }
+
+
+    /*
+     * Holder
+     */
+    {
+      addToVariables("holder_rollIn_voltage", -0.85);
+      addToVariables("holder_rollOut_voltage", 0.9);
+
+      addToVariables("servo_Initial_Angle", 155.0);
+      addToVariables("servo_Final_Angle", 55.0);
+      addToVariables("servo_Work_Time", 300.0); // in ms
+      addToVariables("servo_Elevator_Height_DownLimit", 0.1); // in meters
+      addToVariables("servo_Elevator_Height_UpLimit", 1.2); // in meters
+    }
+
+    /*
+     * Holder + intake sequences
+     */
+    {
+      addToVariables("cubeWait_stall_time", 750.0);
+    }
+
+    /*
+     * Elevator
+     */
+    {
+      addToVariables("elevator_setpoint_bottom", 0.0);
+      addToVariables("elevator_setpoint_switch", 0.8);
+      addToVariables("elevator_setpoint_scale", 1.8);
+      addToVariables("elevator_setpoint_top", 1.92);
+      addToVariables("elevator_level_tolerance", 0.07);
+
+      /*
+       * Elevator to Level
+       */
+      {
+        // PID
+        addToVariables("elevator_PID_KP", 400.0);
+        addToVariables("elevator_PID_KI", 7.0);
+        addToVariables("elevator_PID_KD", 60.0);
+
+        // BANG BANG
+        addToVariables("elevator_PID_Tolerance", 0.025);
+        addToVariables("elevator_BangBang_UpVoltage", 0.65);
+        addToVariables("elevator_BangBang_DownVoltage", -0.3);
+
+        addToVariables("elevator_MoveToEdge_UpVoltage", 0.78);
+
+        addToVariables("elevator_MoveToEdge_DownVoltage", -0.75);
+        addToVariablesA("elevator_MoveToEdge_SlowDownVoltage", -0.2);
+        addToVariablesB("elevator_MoveToEdge_SlowDownVoltage", -0.35);
+        addToVariables("elevator_MoveToEdge_StartDownVoltage", -0.3);
+        addToVariables("elevator_MoveToEdge_StartDownTime", 300.0); // in ms
+        addToVariables("elevator_MoveToEdge_SlowTime", 300.0); // in ms
+
+        // SHAKEN
+        addToVariables("elevator_Shaken_Tolerance", 0.01);
+        addToVariables("elevator_Shaken_DownVoltage", -0.1);
+        addToVariables("elevator_Shaken_UpVoltage", 0.08);
+      }
+
+      /*
+       * ElevatorJoystick
+       */
+      {
+        addToVariables("elevator_Joystick_LowPassVal", 0.2);
+      }
+
+      /*
+       * Climbing
+       */
+      {
+        addToVariables("elevator_ClimbUp_Voltage", -0.9);
+        addToVariables("elevator_ClimbUpStay_Voltage", -0.4);
+      }
+
+      /*
+       * Shifter
+       */
+      {
+        addToVariables("elevator_Shifter_Delay", 1000.0); // in milliseconds
+      }
+    }
+  }
+}
---
+++ src/main/java/org/usfirst/frc/team3316/config/IO.java
@@ -0,0 +1,375 @@
+package org.usfirst.frc.team3316.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IO {
+	public static Map<String, Integer> pwmA = new HashMap<>();
+	public static Map<String, Integer> canA = new HashMap<>();
+
+	public static Map<String, Integer> pwmB = new HashMap<>();
+	public static Map<String, Integer> canB = new HashMap<>();
+
+	public static Map<String, Integer> dioA = new HashMap<>();
+	public static Map<String, Integer> dioB = new HashMap<>();
+
+	public static Map<String, Integer> aioA = new HashMap<>();
+	public static Map<String, Integer> aioB = new HashMap<>();
+
+	public static Map<String, Integer> pdpA = new HashMap<>();
+	public static Map<String, Integer> pdpB = new HashMap<>();
+
+	/**
+	 * Finds the key that is mapped to a specified channel in the parameter map.
+	 *
+	 * @param in
+	 *            The map we want to search in.
+	 * @param value
+	 *            The specified channel.
+	 * @return The key that is mapped to the requested channel. If none exists the
+	 *         method returns null.
+	 */
+	private static Object findKey(Map<?, ?> in, Object value) {
+		for (Object key : in.keySet()) {
+			if (in.get(key).equals(value)) {
+				return key;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Puts a mapping of a key to a channel in the requested map.
+	 *
+	 * @param in
+	 *            The map to add the key.
+	 * @param name
+	 *            The key.
+	 * @param channel
+	 *            The channel to map the key to.
+	 * @throws Exception
+	 *             If the channel already has a mapping, throws an exception.
+	 */
+	private static void put(Map<String, Integer> in, String name, int channel) throws Exception {
+		if (in.containsValue(channel)) {
+			throw new Exception(
+					"Channel " + channel + " for key " + name + " already exists and is: " + findKey(in, channel));
+		}
+
+		in.put(name, channel);
+		Config.addToConstants(name, channel);
+	}
+
+	/**
+	 * Puts a mapping of a key to a channel in the requested map of ROBOT A.
+	 *
+	 * @param in
+	 *            The map to add the key.
+	 * @param name
+	 *            The key.
+	 * @param channel
+	 *            The channel to map the key to.
+	 * @throws Exception
+	 *             If the channel already has a mapping, throws an exception.
+	 */
+	private static void putA(Map<String, Integer> in, String name, int channel) throws Exception {
+		if (in.containsValue(channel)) {
+			throw new Exception(
+					"Channel " + channel + " for key " + name + " already exists and is: " + findKey(in, channel));
+		}
+
+		in.put(name, channel);
+		Config.addToConstantsA(name, channel);
+	}
+
+	/**
+	 * Puts a mapping of a key to a channel in the requested map of ROBOT B.
+	 *
+	 * @param in
+	 *            The map to add the key.
+	 * @param name
+	 *            The key.
+	 * @param channel
+	 *            The channel to map the key to.
+	 * @throws Exception
+	 *             If the channel already has a mapping, throws an exception.
+	 */
+	private static void putB(Map<String, Integer> in, String name, int channel) throws Exception {
+		if (in.containsValue(channel)) {
+			throw new Exception(
+					"Channel " + channel + " for key " + name + " already exists and is: " + findKey(in, channel));
+		}
+
+		in.put(name, channel);
+		Config.addToConstantsB(name, channel);
+	}
+
+	/**
+	 * Put method for pwm channels on robot A. Read the documentation of the put
+	 * method.
+	 */
+	private static void putPWMA(String name, int channel) throws Exception {
+		putA(pwmA, name, channel);
+	}
+
+	/**
+	 * Put method for can channels on robot A. Read the documentation of the put
+	 * method.
+	 */
+	private static void putCANA(String name, int channel) throws Exception {
+		putA(canA, name, channel);
+	}
+
+	/**
+	 * Put method for pwm channels on robot B. Read the documentation of the put
+	 * method.
+	 */
+	private static void putPWMB(String name, int channel) throws Exception {
+		putB(pwmB, name, channel);
+	}
+
+	/**
+	 * Put method for can channels on robot B. Read the documentation of the put
+	 * method.
+	 */
+	private static void putCANB(String name, int channel) throws Exception {
+		putB(canB, name, channel);
+	}
+
+	/**
+	 * Put method for dio channels. Read the documentation of the put method.
+	 */
+	private static void putDIOA(String name, int channel) throws Exception {
+		putA(dioA, name, channel);
+	}
+
+	/**
+	 * Put method for dio channels. Read the documentation of the put method.
+	 */
+	private static void putDIOB(String name, int channel) throws Exception {
+		putB(dioB, name, channel);
+	}
+
+	/**
+	 * Put method for aio channels. Read the documentation of the put method.
+	 */
+	private static void putAIOA(String name, int channel) throws Exception {
+		putA(aioA, name, channel);
+	}
+
+	/**
+	 * Put method for aio channels. Read the documentation of the put method.
+	 */
+	private static void putAIOB(String name, int channel) throws Exception {
+		putB(aioB, name, channel);
+	}
+
+	/**
+	 * Put method for pdp channels. Read the documentation of the put method.
+	 */
+	private static void putPDPA(String name, int channel) throws Exception {
+		putA(pdpA, name, channel);
+	}
+
+	/**
+	 * Put method for pdp channels. Read the documentation of the put method.
+	 */
+	private static void putPDPB(String name, int channel) throws Exception {
+		putB(pdpB, name, channel);
+	}
+
+	public static void initIO() {
+		try {
+			/*
+			 * PWM and CAN initialization
+			 */
+			{
+				/*
+				 * Robot A
+				 */
+				{
+					// Chassis
+					// Left front
+					putCANA("CHASSIS_MOTOR_LEFT_1", 2);
+					// Left back
+					putCANA("CHASSIS_MOTOR_LEFT_2", 3);
+					// Right front
+					putCANA("CHASSIS_MOTOR_RIGHT_1", 12);
+					// Right back
+					putCANA("CHASSIS_MOTOR_RIGHT_2", 13);
+
+					// Intake
+					// Left
+					putPWMA("INTAKE_MOTOR_LEFT", 2);
+					// Right
+					putPWMA("INTAKE_MOTOR_RIGHT", 1);
+
+					// Elevator
+					// Left front
+					putCANA("ELEVATOR_MOTOR_1", 14);
+					// Left back
+					putCANA("ELEVATOR_MOTOR_2", 15);
+
+					// Holder
+					putPWMA("HOLDER_MOTOR", 0);
+					putPWMA("HOLDER_SERVO", 4);
+
+
+					// OTHER
+					Config.addToConstants("PCM_CAN_ID", 1);
+				}
+
+				/*
+				 * Robot B
+				 */
+				{
+					// Chassis
+					// Left front
+					putCANB("CHASSIS_MOTOR_LEFT_1", 13);
+					// Left back
+					putCANB("CHASSIS_MOTOR_LEFT_2", 12);
+					// Right front
+					putCANB("CHASSIS_MOTOR_RIGHT_1", 15);
+					// Right back
+					putCANB("CHASSIS_MOTOR_RIGHT_2", 14);
+
+					// Intake
+					// Left
+					putPWMB("INTAKE_MOTOR_LEFT", 2);
+					// Right
+					putPWMB("INTAKE_MOTOR_RIGHT", 1);
+
+					// Elevator
+					// Left front
+					putCANB("ELEVATOR_MOTOR_1", 2);
+					// Left back
+					putCANB("ELEVATOR_MOTOR_2", 3);
+
+					// Holder
+					putPWMB("HOLDER_MOTOR", 0);
+					putPWMB("HOLDER_SERVO", 3);
+				}
+			}
+
+			/*
+			 * DIO initialization
+			 */
+			{
+				/*
+				 * Robot A
+				 */
+				{
+					// Chassis
+					putDIOA("CHASSIS_LEFT_ENCODER_CHANNEL_A", 0);
+					putDIOA("CHASSIS_LEFT_ENCODER_CHANNEL_B", 1);
+
+					putDIOA("CHASSIS_RIGHT_ENCODER_CHANNEL_A", 3);
+					putDIOA("CHASSIS_RIGHT_ENCODER_CHANNEL_B", 2);
+
+					// Elevator
+					putDIOA("ELEVATOR_ENCODER_CHANNEL_A", 5);
+					putDIOA("ELEVATOR_ENCODER_CHANNEL_B", 4);
+
+					putDIOA("ELEVATOR_BOTTOM_HE", 6);
+					putDIOA("ELEVATOR_BP_BOTTOM_HE", 9);
+					putDIOA("ELEVATOR_BP_TOP_HE", 10);
+					putDIOA("ELEVATOR_TOP_HE", 8);
+
+					// Holder
+					putDIOA("HOLDER_LIMIT_SWITCH", 7);
+				}
+
+				/*
+				 * Robot B
+				 */
+				{
+					// Chassis
+					putDIOB("CHASSIS_LEFT_ENCODER_CHANNEL_A", 9);
+					putDIOB("CHASSIS_LEFT_ENCODER_CHANNEL_B", 8);
+
+					putDIOB("CHASSIS_RIGHT_ENCODER_CHANNEL_A", 7);
+					putDIOB("CHASSIS_RIGHT_ENCODER_CHANNEL_B", 6);
+
+					// Elevator
+					putDIOB("ELEVATOR_ENCODER_CHANNEL_A", 5);
+					putDIOB("ELEVATOR_ENCODER_CHANNEL_B", 4);
+
+					putDIOB("ELEVATOR_BOTTOM_HE", 1);
+					putDIOB("ELEVATOR_BP_BOTTOM_HE", 2);
+					putDIOB("ELEVATOR_BP_TOP_HE", 3);
+					putDIOB("ELEVATOR_TOP_HE", 0);
+
+					// Holder
+					putDIOB("HOLDER_LIMIT_SWITCH", 10);
+				}
+			}
+
+			/*
+			 * AIO initialization
+			 */
+			{
+				/*
+				 * Robot A
+				 */
+				{
+				}
+
+				/*
+				 * Robot B
+				 */
+				{
+				}
+			}
+
+			/*
+			 * PDP initialization
+			 */
+			{
+				/*
+				 * Robot A
+				 */
+				{
+					putPDPA("CHASSIS_MOTOR_LEFT_1_PDP_CHANNEL", 2);
+					putPDPA("CHASSIS_MOTOR_LEFT_2_PDP_CHANNEL", 3);
+					putPDPA("CHASSIS_MOTOR_RIGHT_1_PDP_CHANNEL", 12);
+					putPDPA("CHASSIS_MOTOR_RIGHT_2_PDP_CHANNEL", 13);
+					putPDPA("ELEVATOR_MOTOR_1_PDP_CHANNEL", 14);
+					putPDPA("ELEVATOR_MOTOR_2_PDP_CHANNEL", 15);
+				}
+
+				/*
+				 * Robot B
+				 */
+				{
+					putPDPB("CHASSIS_MOTOR_LEFT_1_PDP_CHANNEL", 13);
+					putPDPB("CHASSIS_MOTOR_LEFT_2_PDP_CHANNEL", 12);
+					putPDPB("CHASSIS_MOTOR_RIGHT_1_PDP_CHANNEL", 15);
+					putPDPB("CHASSIS_MOTOR_RIGHT_2_PDP_CHANNEL", 14);
+					putPDPB("ELEVATOR_MOTOR_1_PDP_CHANNEL", 2);
+					putPDPB("ELEVATOR_MOTOR_2_PDP_CHANNEL", 3);
+				}
+			}
+
+			/*
+			 * PCM initialization
+			 */
+			{
+				/*
+				 * Robot A
+				 */
+				{
+					Config.addToConstants("ELEVATOR_SHIFTER_FORWARD", 0);
+					Config.addToConstants("ELEVATOR_SHIFTER_REVERSE", 1);
+				}
+
+				/*
+				 * Robot B
+				 */
+				{
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
